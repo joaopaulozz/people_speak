db21537d0ca8167679c910f7a234d014a70a584c
João Paulo de Oliveira <joaopaulooliveiira@hotmail.com>
2023-08-22 17:14:43 -0300

    first commit in production

---
+++ README.md
@@ -0,0 +1,38 @@
+# People Speak
+
+Um Assistente Virtual especialmente projetado para websites que desejam incorporar um plugin de acessibilidade, destinado a pessoas com deficiência visual. Ao destacar qualquer trecho de texto ou até mesmo a página inteira, esse plugin possibilita que o conteúdo seja lido em voz alta para o usuário.
+
+## 🚀 Começando
+
+Faça o download do projeto e chame ele no seu HTML, o icone pode ser customizavel:
+    <div class="pessoaLer">
+            <button id="lerTexto" title="Ler Texto">
+                <img src="img/pessoafalar.png" width="35"/>
+            </button>
+        </div>   
+    <script src="js/script.js"></script>
+
+### 📋 Pré-requisitos
+
+Baixar todo o projeto e colar em seu ambiente
+
+## ⚙️ Executando os testes
+
+Selecione o texto que voce deseja ler e clique no icone que vai aparecer no canto direito da tela.
+
+## 📌 Versão
+
+1.0
+
+## ✒️ Autores
+
+* **João Paulo de Oliveira** - *Programador* - joaopaulozz
+## 📄 Licença
+
+Este projeto está sob a licença do autor.
+
+## 🎁 Expressões de gratidão
+
+* Conte a outras pessoas sobre este projeto 📢;
+* Convide o autor para uma cerveja 🍺;
+* Inclusão social;
---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Função para ler o texto selecionado
+function lerTextoSelecionado() {
+    // Obtém o texto selecionado na página
+    const textoSelecionado = window.getSelection().toString();
+
+    // Verifica se algum texto foi selecionado
+    if (textoSelecionado) {
+        // Saudação inicial
+        const saudacao = new SpeechSynthesisUtterance("Ola, sou o JP, seu assistente virtual para leitura, vamos começar.");
+        speechSynthesis.speak(saudacao);
+        // Cria um objeto de "utterance" para a síntese de fala
+        const utterance = new SpeechSynthesisUtterance(textoSelecionado);
+        // Inicia a síntese de fala para ler o texto selecionado
+        speechSynthesis.speak(utterance);
+    } else {
+        // Exibe um alerta se nenhum texto foi selecionado
+        // alert("Selecione um texto para ouvir.");
+        const todoTexto = document.body.innerText;
+        const selecao = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(document.body);
+        selecao.removeAllRanges();
+        selecao.addRange(range);
+        // Saudação inicial
+        const saudacao = new SpeechSynthesisUtterance("Ola, sou o JP, seu assistente virtual para leitura, vamos começar.");
+        speechSynthesis.speak(saudacao);
+        // Cria um objeto de "utterance" para a síntese de fala
+        const utterance = new SpeechSynthesisUtterance(todoTexto);
+        // Inicia a síntese de fala para ler o texto selecionado
+        speechSynthesis.speak(utterance);
+        //alert('Texto selecionado:\n' + todoTexto);
+    }
+}
+
+// Obtém a referência ao botão "Ler Texto Selecionado"
+const botaoLerTexto = document.getElementById('lerTexto');
+
+// Adiciona um ouvinte de eventos ao botão para chamar a função lerTextoSelecionado() quando clicado
+botaoLerTexto.addEventListener('click', lerTextoSelecionado);
+
+// Função a ser executada pelo atalho de teclado
+function executarAtalho() {
+    alert('Atalho de teclado pressionado!'); // Substitua por seu código JavaScript
+}
+
+// Adiciona um evento de escuta ao documento para detectar a combinação de teclas (Ctrl + Shift + F)
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.shiftKey && event.key === 'F') {
+        executarAtalho(); // Chama a função quando a combinação de teclas é detectada
+        // start nas falas
+        lerTextoSelecionado()
+    }
+});
